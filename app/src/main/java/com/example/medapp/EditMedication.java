ba1af511c3e6645fdc5bfb30f71eb12745248c02
aileenrzhang <61919893+aileenrzhang@users.noreply.github.com>
2020-09-01 19:56:13 -0700

    Added Individual Medication Page
    Drafted beginning of the individual medication edit page

---
+++ app/src/main/java/com/example/medapp/EditMedication.java
@@ -0,0 +1,23 @@
+package com.example.medapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class EditMedication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_medication);
+
+        Spinner spinner = (Spinner) findViewById(R.id.frequency_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.frequency_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+    }
+}
--- app/src/main/java/com/example/medapp/MainActivity.java
+++ app/src/main/java/com/example/medapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.medapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Home);
+        setContentView(R.layout.home);
+
+        Button button = (Button)findViewById(R.id.home_swipe_up);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EditMedication.class));
+            }
+        });
     }
 }
