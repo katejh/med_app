8d13a06f3727447382e87d937ce8f5225b1b3117
aileenrzhang <61919893+aileenrzhang@users.noreply.github.com>
2020-09-03 21:50:59 -0700

    Edited Individual Medication page
    Almost finished drafting UI of the individual medication editing page

--- app/src/main/java/com/example/medapp/EditMedicationActivity.java
+++ app/src/main/java/com/example/medapp/EditMedicationActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.NumberPicker;
 import android.widget.Spinner;
 
 public class EditMedicationActivity extends AppCompatActivity {
@@ -13,11 +14,22 @@ public class EditMedicationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_medication);
 
-        Spinner spinner = (Spinner) findViewById(R.id.frequency_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        Spinner frequency_spinner = (Spinner) findViewById(R.id.frequency_spinner);
+        ArrayAdapter<CharSequence> frequecy_adapter = ArrayAdapter.createFromResource(this,
                 R.array.frequency_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        frequecy_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        frequency_spinner.setAdapter(frequecy_adapter);
+
+        Spinner end_date_spinner = (Spinner) findViewById(R.id.end_date_spinner);
+        ArrayAdapter<CharSequence> end_date_adapter = ArrayAdapter.createFromResource(this,
+                R.array.frequency_array, android.R.layout.simple_spinner_item);
+        end_date_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        end_date_spinner.setAdapter(end_date_adapter);
+
+        NumberPicker mednumpicker = findViewById(R.id.mednumpicker);
+
+        mednumpicker.setMaxValue(150);
+        mednumpicker.setMinValue(0);
 
     }
 }
